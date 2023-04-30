bb79f1e014f399032662acbeb502d3036682c223
guillaumeq <guillaumeq@eisti.eu>
2023-04-30 12:26:49 +0200

    Added an API to generate user tokens

---
+++ gen_user_token.py
@@ -0,0 +1,63 @@
+import psycopg2
+import json
+import jwt
+
+from configparser import ConfigParser
+
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+def get_user_token(name, password):
+    # read connection parameters
+    params = config()
+    
+    # connect to the PostgreSQL server
+    conn = psycopg2.connect(**params)
+    
+    # create a cursor
+    cur = conn.cursor()
+        
+    # execute a statement
+    cur.execute(f"SELECT * FROM users  WHERE name = \'{name}\';")
+    # cur.excute(f"genre_ids")
+   
+    # display the query result
+    result = cur.fetchone()
+
+    print(result)
+
+    if(result == None):
+        return "Invalid Account"
+    else :
+        if result[2] != password:
+            return "Invalid Password"
+
+    payload = {
+        'username': result[1],
+        'email': result[3]
+    }
+    
+    secret_key = 'my-secret-key'
+    token = jwt.encode(payload, secret_key, algorithm='HS256')
+
+    print(token)
+
+    cur.close()
+    conn.close()
+    return token
